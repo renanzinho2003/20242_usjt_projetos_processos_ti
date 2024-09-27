f7eb68d7d28229b98c40c5b79f42e8137e6f92db
Renanzinho <renanfern8@gmail.com>
2024-09-26 21:34:48 -0300

    feat: implemena previsao

--- previsao_do_tempo/index.js
+++ previsao_do_tempo/index.js
@@ -1,4 +1,24 @@
 require('dotenv').config()
+const axios = require('axios')
+// const PROTOCOL = process.PROTOCOL
+// const APPID = process.env.APPID
+const{
+    PROTOCOL,
+    BASE_URL,
+    APPID,
+    Q,
+    UNITS,
+    CNT
+    
 
-const APPID = process.env.APPID
-console.log(APPID)
+} = process.env
+
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&q=${Q}&units={UNITS}&cnt=${CNT}`
+
+axios.get(url).then((res) => {
+   // console.log(res.data)
+   for(let previsao of res.data.list){
+    console.log(new Date(previsao.dt * 1000).toLocaleDateString())
+   }
+})
+
